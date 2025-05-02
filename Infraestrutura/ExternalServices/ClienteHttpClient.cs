f9ad07ea5be90bed743c0c8f3adf43793f5a42ee
AnaLe <analuizaalg110@gmail.com>
2025-05-01 21:39:22 -0300

    Finalização CRUD de Cliente

--- Infraestrutura/ExternalServices/ClienteHttpClient.cs
+++ Infraestrutura/ExternalServices/ClienteHttpClient.cs
@@ -14,12 +14,28 @@ namespace Aula05.Infraestrutura.ExternalServices
 
         public async Task<List<Cliente>> BuscarClientesAsync()
         {
-            return await _httpClient.GetFromJsonAsync<List<Cliente>>("https://localhost:44390/api/Cliente") ?? new List<Cliente>();
+            return await _httpClient.GetFromJsonAsync<List<Cliente>>("https://localhost:44328/api/Cliente") ?? new List<Cliente>();
         }
 
         public async Task AtualizarClienteAsync(Cliente cliente)
         {
-            await _httpClient.PutAsJsonAsync($"https://localhost:44390/api/Cliente/{cliente.Id}", cliente);
+            await _httpClient.PutAsJsonAsync($"https://localhost:44328/api/Cliente/{cliente.Id}", cliente);
+        }
+
+        public async Task<Cliente> ObterClienteAsync(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<Cliente>($"https://localhost:44328/api/Cliente/{id}") ?? new Cliente
+                ();
+        }
+
+        public async Task ExcluirClienteAsync(int id)
+        {
+            await _httpClient.DeleteAsync($"https://localhost:44328/api/Cliente/{id}");
+        }
+
+        public async Task CriarClienteAsync(Cliente cliente)
+        {
+            await _httpClient.PostAsJsonAsync("https://localhost:44328/api/Cliente", cliente);
         }
     }
 }
